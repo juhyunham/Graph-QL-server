de9688dc4895de044343357f98f2a26a5c4e8a0b
juhyunham <judev0227@gmail.com>
2022-02-05 18:34:47 +0900

    return data

--- graphql/movie_db.js
+++ graphql/movie_db.js
@@ -1,8 +1,7 @@
 import fetch from "node-fetch";
 const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getMovies = (limit, rating) => {
+export const getMovies = (limit, rating) =>
   fetch(`${API_URL}`)
     .then((res) => res.json())
     .then((json) => json.data.movies);
-};
