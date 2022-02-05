49141338d3797155e36ab23878afa90efdde00a2
juhyunham <judev0227@gmail.com>
2022-02-05 18:24:45 +0900

    REST API With GraphQL - movie data

---
+++ graphql/movie_db.js
@@ -0,0 +1,8 @@
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
+
+export const getMovies = (limit, rating) => {
+  fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./movie_db";
 
 const resolvers = {
   Query: {
     movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
